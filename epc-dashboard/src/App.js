80b68ba4d0c36857194310b6c5cf138c8b4d7b90
Jack Armstrong <jarmstrong56@qub.ac.uk>
2024-11-08 14:52:47 +0000

    matthews changes

--- epc-dashboard/src/App.js
+++ epc-dashboard/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import profileIcon from './assets/profileicon.png'; // Adjust path as needed
 import Login from './Login'; // Make sure these paths are correct
 import Register from './Register';
+import PropertyPage from './Components/PropertyPage'; // Add the PropertyPage import
 
 function App() {
   const [dropdownVisible, setDropdownVisible] = useState(false);
@@ -19,7 +20,7 @@ function App() {
           <h1 className="app-title">EP<i>City</i></h1>
           <div className="profile-icon" onClick={toggleDropdown}>
             <img src={profileIcon} alt="Profile" className="profile-img" />
-            {dropdownVisible && (  //adding in profile icon, as well as a drop down that when clicked will display login and register
+            {dropdownVisible && (
               <div className="dropdown-menu">
                 <Link to="/login">Login</Link>
                 <Link to="/register">Register</Link>
@@ -40,29 +41,26 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/property" element={<PropertyPage />} /> {/* Add the route for PropertyPage */}
         </Routes>
 
         <div className="table-container">
-          <h2>Table Area</h2>
+          <h2>Property Information</h2>
           <div className="table-border">
             <table>
-              <thead>
-                <tr>
-                  <th>Header 1</th>
-                  <th>Header 2</th>
-                  <th>Header 3</th>
-                </tr>
-              </thead>
               <tbody>
                 <tr>
-                  <td>Row 1 Col 1</td>
-                  <td>Row 1 Col 2</td>
-                  <td>Row 1 Col 3</td>
-                </tr>
-                <tr>
-                  <td>Row 2 Col 1</td>
-                  <td>Row 2 Col 2</td>
-                  <td>Row 2 Col 3</td>
+                  <td>
+                    {/* Pass the address as state to PropertyPage */}
+                    <Link to="/property" state={{ address: "44 Gladstone Court, Spring Drive SG2 8AY" }}>
+                      30 Sep 2024
+                    </Link>
+                  </td>
+                  <td>
+                    <Link to="/property" state={{ address: "44 Gladstone Court, Spring Drive SG2 8AY" }}>
+                      44 Gladstone Court, Spring Drive SG2 8AY
+                    </Link>
+                  </td>
                 </tr>
               </tbody>
             </table>
