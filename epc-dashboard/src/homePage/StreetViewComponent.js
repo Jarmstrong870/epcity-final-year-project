8c7a89468dd7ca894ee7a9e0fdd445b4a36f549d
40334034 <ckennedy744@qub.ac.uk>
2025-03-10 13:33:59 +0000

    Changes made to propertyCard

--- epc-dashboard/src/homePage/StreetViewComponent.js
+++ epc-dashboard/src/homePage/StreetViewComponent.js
@@ -54,7 +54,7 @@ const StreetViewComponent = ({ address, postcode, propertyType }) => {
         <img
           src={streetViewURL}
           alt="Street View"
-          style={{ width: "100%", height: "100%", borderRadius: "10px", objectFit: "cover" }}
+          
         />
       ) : placeholderImage ? (
         <img
