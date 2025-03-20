c22d5c8cfe3fd61e223be8da7fd3127314c7c66b
40334034 <ckennedy744@qub.ac.uk>
2025-03-20 01:00:27 +0000

    addition of a pop up message if a user is not signed in and trys to favourite a property

--- epc-dashboard/src/propertySearch/FavouriteStar.js
+++ epc-dashboard/src/propertySearch/FavouriteStar.js
@@ -1,34 +1,50 @@
 import React, { useEffect, useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FavouriteContext } from '../Components/utils/favouriteContext';
+import './FavouriteStar.css';
 
 const FavouriteStar = ({ user, property }) => {
   const { addFavourite, removeFavourite, isFavourited, favouriteProperties } = useContext(FavouriteContext);
+  const navigate = useNavigate();
   const uprn = property?.uprn ? String(property.uprn) : "";
-
   const [isLoading, setIsLoading] = useState(false);
   const [propertyFavourited, setIsPropertyFavourited] = useState(isFavourited(uprn));
+  const [popupMessage, setPopupMessage] = useState('');
+  const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
     setIsPropertyFavourited(isFavourited(uprn));
-  }, [favouriteProperties]);
+  }, [favouriteProperties, uprn, isFavourited]);
 
-  const toggleFavourite = async () => {
-    if (!user?.email || isLoading) return;
-
+  const toggleFavourite = async (e) => {
+    e.stopPropagation();
+    if (!user?.email) {
+      setPopupMessage(
+        <>
+          Please log in or sign up
+          <br />
+          to favourite properties
+        </>
+      );
+      
+      setShowPopup(true);
+      setTimeout(() => setShowPopup(false), 3000);
+      return;
+    }
+    if (isLoading) return;
     setIsLoading(true);
-
     if (propertyFavourited) {
       await removeFavourite(property);
     } else {
-      addFavourite(user, property)
+      await addFavourite(user, property);
     }
-
     setIsPropertyFavourited(!propertyFavourited);
     setIsLoading(false);
   };
 
   return (
     <div className="starBase" onClick={toggleFavourite} title={propertyFavourited ? 'Click to unfavourite' : 'Click to favourite'}>
+      {showPopup && <div className="popup">{popupMessage}</div>}
       <span className="heart-emoji">
         {propertyFavourited ? "❤️" : "🤍"}
       </span>
